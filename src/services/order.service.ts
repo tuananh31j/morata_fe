5bd01109c1d80d796a1fa833fd7bcad7189f2ca0
Tuan Anh <tuananh31j@gmail.com>
2024-07-01 14:05:21 +0700

    chore: change path from service get all orders

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,7 +1,7 @@
 import instance from '~/utils/api/axiosIntance';
 import { IAxiosResponse } from '~/types/AxiosResponse';
 import { ORDER_ENDPOINT } from '~/constants/endpoint';
-import { IOrder, IOrderDetails } from '~/types/Order';
+import { IOrder } from '~/types/Order';
 import { OrderStatus } from '~/types/enum';
 
 const orderService = {
@@ -9,7 +9,7 @@ const orderService = {
         return instance.get<IAxiosResponse<IOrder[]>>(`${ORDER_ENDPOINT.MY_ORDERS}`);
     },
     getAllOrders() {
-        return instance.get<IAxiosResponse<IOrder[]>>(`${ORDER_ENDPOINT.MY_ORDERS}`);
+        return instance.get<IAxiosResponse<IOrder[]>>(`${ORDER_ENDPOINT.ROOT}`);
     },
     cancelOrder(id: string) {
         return instance.patch<void, { orderId: string }>(`${ORDER_ENDPOINT.CANCELED}`, {
@@ -22,7 +22,7 @@ const orderService = {
         });
     },
     orderDetails(id: string) {
-        return instance.get<IAxiosResponse<Omit<IOrderDetails, '_id'>>>(`${ORDER_ENDPOINT.ROOT}/${id}`);
+        return instance.get<IAxiosResponse<Omit<IOrder, '_id'>>>(`${ORDER_ENDPOINT.ROOT}/${id}`);
     },
     orderStatus() {
         return instance.get<IAxiosResponse<OrderStatus[]>>(`${ORDER_ENDPOINT.MY_ORDERS}`);
