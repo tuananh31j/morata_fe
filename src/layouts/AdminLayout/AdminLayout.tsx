3973f51d8583c64b221becdc118de380f82bb563
Tuan Anh <tuananh31j@gmail.com>
2024-08-21 00:07:43 +0700

    chore: add title page admin

--- src/layouts/AdminLayout/AdminLayout.tsx
+++ src/layouts/AdminLayout/AdminLayout.tsx
@@ -2,8 +2,11 @@ import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../_components/Admin/Sidebar';
 import Header from '../_components/Admin/Header/Header';
+import useDocumentTitle from '~/hooks/_common/useDocumentTitle';
 
 export default function AdminLayout() {
+    useDocumentTitle('Quản trị');
+
     const [sidebarOpen, setSidebarOpen] = useState(false);
     return (
         <div className='flex h-screen overflow-hidden bg-[#F5F5F5] dark:bg-slate-700 '>
