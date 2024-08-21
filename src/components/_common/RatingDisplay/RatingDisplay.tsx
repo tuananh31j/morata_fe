9a8428ebde8335961ef1eafefcbfaf55aeba0feb
QuocDL <145692924+QuocDL@users.noreply.github.com>
2024-08-21 10:21:37 +0700

    chore: fix card product

--- src/components/_common/RatingDisplay/RatingDisplay.tsx
+++ src/components/_common/RatingDisplay/RatingDisplay.tsx
@@ -6,7 +6,7 @@ const RatingDisplay = ({ rating, reviews }: { rating: number; reviews?: number }
     return (
         <div className={cn('mt-[10px] items-center gap-1', { ['flex']: !reviews || reviews < 100 })}>
             <Rate className='text-[12px] text-[#FFB800]' allowHalf disabled={true} defaultValue={checkRating} />
-            {reviews && <span className='text-gray-500 text-[12px]'>{`(${reviews})`}</span>}
+            {reviews !== 0 && <span className='text-gray-500 text-[12px]'>{`(${reviews})`}</span>}
         </div>
     );
 };
