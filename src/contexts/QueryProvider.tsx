bfb006704fe328d32f261577f2416d6a07445481
Tuan Anh <anhntph30476@fpt.edu.vn>
2025-07-08 01:56:26 +0700

    Update QueryProvider.tsx
    Signed-off-by: Tuan Anh <anhntph30476@fpt.edu.vn>

--- src/contexts/QueryProvider.tsx
+++ src/contexts/QueryProvider.tsx
@@ -21,7 +21,7 @@ const QueryProvider = ({ children }: { children?: React.ReactNode }) => {
     return (
         <QueryClientProvider client={queryClient}>
             {children}
-            <ReactQueryDevtools initialIsOpen={false} />
+{/*             <ReactQueryDevtools initialIsOpen={false} /> */}
         </QueryClientProvider>
     );
 };
